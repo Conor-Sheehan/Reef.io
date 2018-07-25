163805479fcd6cf350c20aec67a0c6c6189efe5e
Conor Sheehan <conor@infinitry.io>
2018-07-25 18:10:56 -0400

    Initial Commit

---
+++ Reef/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reef
+//
+//  Created by Conor Sheehan on 7/25/18.
+//  Copyright © 2018 Infinitry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
